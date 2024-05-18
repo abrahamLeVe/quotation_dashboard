f35cce58f476e871ba4d766b18e69c046ff04444
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2024-05-18 04:12:36 -0500

    added panel admin

--- src/app/(dashboard)/panel/page.tsx
+++ src/app/(dashboard)/panel/page.tsx
@@ -1,7 +1,42 @@
-import { getServerSession } from "next-auth/next";
-import { options } from "@/app/api/auth/[...nextauth]/options";
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { Mail } from "./components/mail";
+import { accounts, mails } from "./data";
 
-export default async function Page() {
-  const session = await getServerSession(options);
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
+export default function MailPage() {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/mail-dark.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="hidden dark:block"
+        />
+        <Image
+          src="/examples/mail-light.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="block dark:hidden"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
+  );
 }
